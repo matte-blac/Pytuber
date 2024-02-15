6f457c29b4cdf2ce09546db163457af562ca7119
matt-blac <bomettmat2@gmail.com>
2024-02-15 11:17:32 +0300

    Complete Implement

--- downloader.py
+++ downloader.py
@@ -1,97 +1,94 @@
-# import required modules
+# Import Required Modules 
 from tkinter import *
-from pyyoutube import Api
-from pytube import YouTube
-from threading import Thread
-from tkinter import messagebox
+from pyyoutube import Api 
+from pytube import YouTube 
+from threading import Thread 
+from tkinter import messagebox 
 
-# it will give a list of all video links of a YouTube playlist
-def get_list_videos():
-    global playlist_item_by_id
-    # clear ListBox
-    list_box.delete(0, 'end')
 
-    # create API object
-    api = Api(api_key='AIzaSyD1rGg-L5CzUyrKsO4wY48VbfxYGC3_RYc')
+def get_list_videos(): 
+	global playlist_item_by_id 
+	# Clear ListBox 
+	list_box.delete(0, 'end') 
 
-    if 'youtube' in playlistId.get():
-        playlist_id = playlistId.get()[len(
-            'https://www.youtube.com/playlist?list='
-        ):]
-    else:
-        playlist_id = playlistId.get()
+	# Create API Object 
+	api = Api(api_key='Enter API Key') 
 
-    # get list of video links
-    playlist_item_by_id = api.get_playlist_items(
-        playlist_id=playlist_id, count=None, return_json=True
-        )
-    
-    # iterate through all video links and insert into listbox
-    for index, videoid in enumerate(playlist_item_by_id['items']):
-        list_box.insert(
-            END, f' {str(index+1)}. {videoid['[contentDetails]']['videoId']}'
-        )
+	if "youtube" in playlistId.get(): 
+		playlist_id = playlistId.get()[len( 
+			"https://www.youtube.com/playlist?list="):] 
+	else: 
+		playlist_id = playlistId.get() 
 
-    download_start.config(state=NORMAL)
+	# Get list of video links 
+	playlist_item_by_id = api.get_playlist_items( 
+		playlist_id=playlist_id, count=None, return_json=True) 
 
-# it is used for threading into tkinter
-def threading():
-    # call download_videos function
-    t1 = Thread(target=download_videos)
-    t1.start()
+	# Iterate through all video links and insert into listbox 
+	for index, videoid in enumerate(playlist_item_by_id['items']): 
+		list_box.insert( 
+			END, f" {str(index+1)}. {videoid['contentDetails']['videoId']}") 
 
-# it is used for downloading youtube videos
-def download_videos():
-    download_start.config(state='disabled')
-    get_videos.config(state='disabled')
+	download_start.config(state=NORMAL) 
 
-    # iterate through all seleced videos
-    for i in list_box.curselection():
-        videoid = playlist_item_by_id['items'][i]['contentDetails']['videoId']
 
-        link = f'https://www.youtube.com/watch?v={videoid}'
+def threading(): 
+	# Call download_videos function 
+	t1 = Thread(target=download_videos) 
+	t1.start() 
 
-        yt_obj = YouTube(link)
 
-        filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
+def download_videos(): 
+	download_start.config(state="disabled") 
+	get_videos.config(state="disabled") 
 
-        # download the highest quality video
-        filters.get_highest_resolution().download()
+	# Iterate through all selected videos 
+	for i in list_box.curselection(): 
+		videoid = playlist_item_by_id['items'][i]['contentDetails']['videoId'] 
 
-    messagebox.showinfo('Success', 'Video Successfully Downloaded')
-    download_start.config(state='normal')
-    get_videos.config(state='normal')
+		link = f"https://www.youtube.com/watch?v={videoid}"
 
-# create object
-root = Tk()
+		yt_obj = YouTube(link) 
 
-# set geometry
-root.geometry('400x400')
+		filters = yt_obj.streams.filter(progressive=True, file_extension='mp4') 
 
-# add label
-Label(root, text='Youtube Playlist Downloader',
-      font='italic 15 bold').pack(pady=10)
-Label(root, text='Enter Playlist URL:', font='italic 10').pack()
+		# download the highest quality video 
+		filters.get_highest_resolution().download() 
 
-# add entry box
-playlistId = Entry(root, width=60)
-playlistId.pack(pady=5)
+	messagebox.showinfo("Success", "Video Successfully downloaded") 
+	download_start.config(state="normal") 
+	get_videos.config(state="normal") 
 
-# add video button
-get_videos = Button(root, text='Get Videos')
-get_videos.pack(pady=10)
 
-# add scrollbar
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=BOTH)
-list_box = Listbox(root, selectmode='multiple')
-list_box.pack(expand=YES, fill='both')
-list_box.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=list_box.yview)
+# Create Object 
+root = Tk() 
+# Set geometry 
+root.geometry('400x400') 
 
-# add download button
-download_start = Button(root, text='Download Start', command=threading, state=DISABLED)
-download_start.pack(pady=10)
+# Add Label 
+Label(root, text="Youtube Playlist Downloader", 
+	font="italic 15 bold").pack(pady=10) 
+Label(root, text="Enter Playlist URL:-", font="italic 10").pack() 
 
-# execute tkinter
-root.mainloop()
+# Add Entry box 
+playlistId = Entry(root, width=60) 
+playlistId.pack(pady=5) 
+
+# Add Button 
+get_videos = Button(root, text="Get Videos", command=get_list_videos) 
+get_videos.pack(pady=10) 
+
+# Add Scrollbar 
+scrollbar = Scrollbar(root) 
+scrollbar.pack(side=RIGHT, fill=BOTH) 
+list_box = Listbox(root, selectmode="multiple") 
+list_box.pack(expand=YES, fill="both") 
+list_box.config(yscrollcommand=scrollbar.set) 
+scrollbar.config(command=list_box.yview) 
+
+download_start = Button(root, text="Download Start", 
+						command=threading, state=DISABLED) 
+download_start.pack(pady=10) 
+
+# Execute Tkinter 
+root.mainloop() 
